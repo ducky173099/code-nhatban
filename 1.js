67e188a7e1c9f550d621115491719b84335a849b
ducky173099 <ducky173099@gmail.com>
2019-09-23 20:59:29 +0700

    init commit

--- 1.js
+++ 1.js
@@ -6,16 +6,25 @@
 //nut menu navbar 
 var btnmenu = document.getElementById('_open');
 var openmenu = document.getElementById('global_navigation');
+var changeicon1 = document.getElementById('icon');
+var changeicon2 = document.getElementById('icontren');
+var changeicon3 = document.getElementById('iconduoi');
 var trangthai = "lan1";
 btnmenu.onclick = function(){
 	if (trangthai == "lan1") {
 		console.log("lan click 1");
 		trangthai = "lan2";
 		openmenu.classList.add("menura");
+		changeicon1.classList.add("hideicon");
+		changeicon2.classList.add("degicon");
+		changeicon3.classList.add("degicon2");
 	} else if (trangthai == "lan2") {
 		console.log("lan click 2");
 		trangthai = "lan1";
 		openmenu.classList.remove("menura");
+		changeicon1.classList.remove("hideicon");
+		changeicon2.classList.remove("degicon");
+		changeicon3.classList.remove("degicon2");
 	}
 };
 
--- btnnav.js
+++ btnnav.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded',function(){
 
-	var btnra = document.getElementById('btnmore');
-	var navra = document.getElementById('navmore');
-	var trangthai = "lan1";
 
+	var trangthai = "lan1";
+	var btnlang = document.getElementById('btnlang');
+	var language = document.getElementById('global_language');
+
+
+	btnlang.onclick = function(){
+		if (trangthai == "lan1") {
+			console.log('lan 1');
+			trangthai = "lan2";
+			language.classList.add('lang_open');
+		} else if(trangthai == "lan2"){
+			console.log('lan 2');
+			trangthai = "lan1";
+			language.classList.remove('lang_open');
+		}
+	}
 
 	// for (var i = 0; i < btnra.length; i++) {
 	// 	btnra[i].addEventListener('click',function(){
@@ -35,23 +48,27 @@ document.addEventListener('DOMContentLoaded',function(){
 
 	// }
 
-	btnra.onclick = function(){
-		// console.log("lan 1");
 
+	var btnra = document.getElementById('btnmore');
+	var navra = document.getElementById('navmore');
+	var iconxoay1 = document.getElementById('iconbtnmoredeg1');
+	btnra.onclick = function(){
 		if (trangthai == "lan1") {
 			console.log('lan 1');
 			trangthai = "lan2";
 			navra.classList.add('navra');
+			iconxoay1.classList.add('iconxoay');
 		} else if(trangthai == "lan2"){
 			console.log('lan 2');
 			trangthai = "lan1";
 			navra.classList.remove('navra');
+			iconxoay1.classList.remove('iconxoay');
 		}
 	}
 
 	var btnra2 = document.getElementById('btnmore2');
 	var navra2 = document.getElementById('navmore2');
-
+	var iconxoay2 = document.getElementById('iconbtnmoredeg2');
 	btnra2.onclick = function(){
 		// console.log("lan 1");
 
@@ -59,16 +76,18 @@ document.addEventListener('DOMContentLoaded',function(){
 			console.log('lan 1');
 			trangthai = "lan2";
 			navra2.classList.add('navra');
+			iconxoay2.classList.add('iconxoay');
 		} else if(trangthai == "lan2"){
 			console.log('lan 2');
 			trangthai = "lan1";
 			navra2.classList.remove('navra');
+			iconxoay2.classList.remove('iconxoay');
 		}
 	}
 
 	var btnra3 = document.getElementById('btnmore3');
 	var navra3 = document.getElementById('navmore3');
-
+	var iconxoay3 = document.getElementById('iconbtnmoredeg3');
 	btnra3.onclick = function(){
 		// console.log("lan 1");
 
@@ -76,16 +95,18 @@ document.addEventListener('DOMContentLoaded',function(){
 			console.log('lan 1');
 			trangthai = "lan2";
 			navra3.classList.add('navra');
+			iconxoay3.classList.add('iconxoay');
 		} else if(trangthai == "lan2"){
 			console.log('lan 2');
 			trangthai = "lan1";
 			navra3.classList.remove('navra');
+			iconxoay3.classList.remove('iconxoay');
 		}
 	}
 
 	var btnra4 = document.getElementById('btnmore4');
 	var navra4 = document.getElementById('navmore4');
-
+	var iconxoay4 = document.getElementById('iconbtnmoredeg4');
 	btnra4.onclick = function(){
 		// console.log("lan 1");
 
@@ -93,16 +114,18 @@ document.addEventListener('DOMContentLoaded',function(){
 			console.log('lan 1');
 			trangthai = "lan2";
 			navra4.classList.add('navra');
+			iconxoay4.classList.add('iconxoay');
 		} else if(trangthai == "lan2"){
 			console.log('lan 2');
 			trangthai = "lan1";
 			navra4.classList.remove('navra');
+			iconxoay4.classList.remove('iconxoay');
 		}
 	}
 
 	var btnra5 = document.getElementById('btnmore5');
 	var navra5 = document.getElementById('navmore5');
-
+	var iconxoay5 = document.getElementById('iconbtnmoredeg5');
 	btnra5.onclick = function(){
 		// console.log("lan 1");
 
@@ -110,13 +133,14 @@ document.addEventListener('DOMContentLoaded',function(){
 			console.log('lan 1');
 			trangthai = "lan2";
 			navra5.classList.add('navra');
+			iconxoay5.classList.add('iconxoay');
 		} else if(trangthai == "lan2"){
 			console.log('lan 2');
 			trangthai = "lan1";
 			navra5.classList.remove('navra');
+			iconxoay5.classList.remove('iconxoay');
 		}
 	}
-
 
 
 
